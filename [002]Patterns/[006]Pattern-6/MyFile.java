8825becebb3bf90601c1d73e2e3c455374dee694
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2023-05-01 13:31:29 +0530

    Module-2 Printing Patterns

---
+++ [002]Patterns/[006]Pattern-6/MyFile.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class MyFile
+{
+    static void printpattern(int rows)
+    {
+        char ch = 'A';
+        for(int i=rows; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int r = sc.nextInt();
+        System.out.println("Output Pattern:");
+        printpattern(r);
+        sc.close();    
+    }
+}
