22f8a5da996ffec11e2d496868cf67f239288440
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2023-05-27 13:23:39 +0530

    Module-6 Strings

---
+++ [006]Strings/[004] Strings charAt Method/MyFile.java
@@ -0,0 +1,13 @@
+public class MyFile
+{
+    public static void main(String[] args) 
+    {
+        String str = "Tony Stark - Iron Man";
+        
+        for(int i=0; i<str.length(); i++)
+        {
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+}
