c28b7763d49a8f49467eafe8c407276ce95a1b87
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2023-05-27 13:16:00 +0530

    Module-6 Strings

---
+++ [006]Strings/[002] Strings Length/MyFile.java
@@ -0,0 +1,9 @@
+public class MyFile
+{
+    public static void main(String[] args) 
+    {
+        String str = "Hello I am PVB-GEEK !!";
+        System.out.println("Length of String is : " + str.length());
+        // .length() function ......
+    }
+}
